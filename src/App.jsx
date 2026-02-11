4c0eb7205d72e31fb2434989f73ac8af79e25adc
Maarten Schuilenburg <m.schuilenburg93@gmail.com>
2026-02-11 21:20:26 +0100

    eerste stapjes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import './App.css'
+import "./App.css";
+import Button from "./components/Button.jsx";
 
 function App() {
+  // const logInConsole = (message) => {
+  //   console.log(message);
+  // };
+
   return (
-      <h1>Handbags & Purses - Test</h1>
-  )
+    <div>
+      <h1>Handbags & Purses</h1>
+      <nav>
+        <ul>
+          {/* <button href="/" onClick={() => logInConsole("to the collection")}>
+            to the collection
+          </button>
+          <button href="/" onClick={() => logInConsole("shop all bags")}>
+            shop all bags
+          </button>
+          <button href="/" onClick={() => logInConsole("pre-orders")} disabled>
+            pre-orders
+          </button>
+          <br></br> */}
+          <Button text="to the collection" />
+          <Button text="shop all bags" />
+          <Button text="pre-orders" disabled={true} />
+        </ul>
+      </nav>
+
+      <main>
+        <h2>Our Collection</h2>
+        {/* <p>Discover our latest handbags and purses.</p> */}
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Button = ({ text, disabled = false }) => {
+  const logInConsole = (message) => {
+    console.log(message);
+  };
+
+  return <button onClick={() => logInConsole(text)} disabled={disabled}>{text}</button>;
+};
+export default Button;
